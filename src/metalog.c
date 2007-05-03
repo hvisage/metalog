72978b53dc696ee1173296759bdc351df91ce6c5
Mike Frysinger <vapier@gentoo.org>
2007-05-03 06:58:31 +0000

    [1655165] if malloc() failed, then just return and wait to dequeue later
    git-svn-id: https://metalog.svn.sourceforge.net/svnroot/metalog/trunk@114 f9d656ae-0c26-0410-9339-c0ec9f5f0137

--- src/metalog.c
+++ src/metalog.c
@@ -1240,6 +1240,8 @@ static void signal_doLog_dequeue(void)
     assert(ret == sizeof(fmt_len));
 
     buf = malloc(fmt_len+1);
+    if (!buf)
+        return;
     ret = read(dolog_queue[0], buf, fmt_len);
     assert(ret == fmt_len);
     buf[fmt_len] = '\0';

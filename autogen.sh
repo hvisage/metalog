9daddbdafe4171cb18d81e094b4f7c25f9261990
Mike Frysinger <vapier@gentoo.org>
2009-01-04 20:31:57 +0000

    import gnulib
    git-svn-id: https://metalog.svn.sourceforge.net/svnroot/metalog/trunk@131 f9d656ae-0c26-0410-9339-c0ec9f5f0137

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,39 @@
 #!/bin/sh
-exec autoreconf -i -f
+
+PATH=/usr/local/src/gnu/gnulib:${PATH}
+gnulib-tool --import \
+	alloca \
+	c-ctype \
+	connect \
+	dirent \
+	errno \
+	fcntl \
+	getopt \
+	inttypes \
+	memcmp \
+	memcpy \
+	memmove \
+	memset \
+	pathmax \
+	poll \
+	printf-posix \
+	putenv \
+	signal \
+	snprintf-posix \
+	socket \
+	socklen \
+	stdarg \
+	stdbool \
+	stdint \
+	string \
+	strdup-posix \
+	strtoull \
+	sys_stat \
+	sys_time \
+	sys_wait \
+	time \
+	unistd \
+	vfprintf-posix \
+	vsnprintf-posix
+
+autoreconf -i -f
